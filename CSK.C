1016581bab4c219d3448c53a8153e044c0978ab4
krish vekriya <131762007+krishvekriya12@users.noreply.github.com>
2023-04-29 11:41:56 +0530

    Add files via upload

---
+++ CSK.C
@@ -0,0 +1,57 @@
+#include<stdio.h>
+#include<conio.h>
+main()
+{
+	clrscr();
+	int a=10,b=15;
+	if(b>a)
+	{
+	    printf("b is minimum");
+	}
+	else
+	{
+	    printf("a is minimum");
+	}
+	getch();
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
